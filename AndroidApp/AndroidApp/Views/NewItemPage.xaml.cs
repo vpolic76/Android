ec01a5ef1dfeb63e47301ee62502a25fa8ac3730
vpolic76 <95443395+vpolic76@users.noreply.github.com>
2022-01-31 15:07:41 +0100

    base

---
+++ AndroidApp/AndroidApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AndroidApp.Models;
+using AndroidApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
